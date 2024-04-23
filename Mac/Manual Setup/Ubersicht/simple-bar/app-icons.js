5856965217da75dc87f19a03ae4e771d8f78d0af
Siming MBP <simingrox@gmail.com>
2024-04-23 01:59:24 -0400

    added app-icons

---
+++ Mac/Manual Setup/Ubersicht/simple-bar/app-icons.js
@@ -0,0 +1,7 @@
+// My own apps
+"Docker Desktop": Icons.Docker,
+"Epic Games Launcher": Icons.EpicGames,
+"iA Writer": Icons.iAWriter,
+IINA: Icons.Mpv,
+"Steam Helper": Icons.Steam,
+"WeChat": Icons.Wechat,
