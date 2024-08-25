130c17b0c80c52862cce7ad6b8919b00d21c73ca
Siming MBP <simingrox@gmail.com>
2024-08-25 19:41:12 -0400

    ubersicht simplifcation

--- Mac/Manual Setup/Ubersicht/simple-bar/app-icons.js
+++ Mac/Manual Setup/Ubersicht/simple-bar/app-icons.js
@@ -1,7 +1,3 @@
 // My own apps
-"Docker Desktop": Icons.Docker,
-"Epic Games Launcher": Icons.EpicGames,
-"iA Writer": Icons.iAWriter,
 IINA: Icons.Mpv,
-"Steam Helper": Icons.Steam,
 "WeChat": Icons.Wechat,
