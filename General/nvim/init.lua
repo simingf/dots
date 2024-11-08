8136f0ab67f134272c2fd45a1d86a53b7f0a280d
Siming MBP <simingrox@gmail.com>
2024-11-07 22:36:07 -0500

    switch from isort and black to ruff

--- General/nvim/init.lua
+++ General/nvim/init.lua
@@ -519,7 +519,7 @@ require("lazy").setup({
         config = function()
             require("conform").setup({
                 formatters_by_ft = {
-                    python = { "isort", "black" },
+                    python = { "ruff" },
                     javascript = { "prettierd" },
                     typescript = { "prettierd" },
                     javascriptreact = { "prettierd" },
