9b5d3c2990ebf45f7e0642d8f7792c5ccbe40619
Siming MBP <simingrox@gmail.com>
2024-11-09 16:36:33 -0500

    added noice and notify

--- General/nvim/init.lua
+++ General/nvim/init.lua
@@ -185,6 +185,52 @@ require("lazy").setup({
         end
     },
 
+    -- noice: improved UI
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true,         -- use a classic bottom cmdline for search
+                    command_palette = true,       -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+                },
+            })
+        end
+    },
+
+    -- notifications
+    {
+        'rcarriga/nvim-notify',
+        config = function()
+            require('notify').setup({
+                background_colour = "#000000",
+                render = "wrapped-compact",
+                minimum_width = 50,
+                max_width = 100,
+                stages = "fade_in_slide_out",
+                timeout = 0,
+            })
+        end
+    },
+
     -- status line at bottom
     {
         'nvim-lualine/lualine.nvim',
