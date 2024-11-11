9f4d0b49c288f097e6a22b445f3a269d0978a56a
Siming MBP <simingrox@gmail.com>
2024-11-11 16:52:46 -0500

    formatting

--- General/nvim/init.lua
+++ General/nvim/init.lua
@@ -313,24 +313,38 @@ require("lazy").setup({
         event = "VeryLazy",
         opts = {},
         keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+            {
+                "s",
+                mode = { "n", "x", "o" },
+                function() require("flash").jump() end,
+                desc = "Flash"
+            },
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function() require("flash").treesitter() end,
+                desc = "Flash Treesitter"
+            },
+            {
+                "r",
+                mode = "o",
+                function() require("flash").remote() end,
+                desc = "Remote Flash"
+            },
+            {
+                "R",
+                mode = { "o", "x" },
+                function() require("flash").treesitter_search() end,
+                desc = "Treesitter Search"
+            },
+            {
+                "<c-s>",
+                mode = { "c" },
+                function() require("flash").toggle() end,
+                desc = "Toggle Flash Search"
+            },
         },
     },
-
-    -- leap: easy file navigation with 's'
-    -- {
-    --     'ggandor/leap.nvim',
-    --     config = function()
-    --         require('leap').create_default_mappings()
-    --         require('leap').opts.special_keys.prev_target = '<bs>'
-    --         require('leap').opts.special_keys.prev_group = '<bs>'
-    --         require('leap.user').set_repeat_keys('<cr>', '<bs>')
-    --     end
-    -- },
 
     -- arrow: bookmark files and locations in files
     {
