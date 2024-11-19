4e799fb6d6067506fb9c92b5e990089e6f8e62a3
Siming <SimingRox@gmail.com>
2024-11-19 11:51:41 -0500

    lualine config

--- General/nvim/init.lua
+++ General/nvim/init.lua
@@ -240,26 +240,59 @@ require("lazy").setup({
         config = function()
             require('lualine').setup({
                 options = {
-                    theme = 'catppuccin',
                     icons_enabled = true,
-                    section_separators = '',
+                    theme = 'catppuccin',
+                    -- component_separators = { left = '', right = '' },
                     component_separators = '|',
+                    -- section_separators = { left = '', right = '' },
+                    section_separators = '',
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    always_show_tabline = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 100,
+                        tabline = 100,
+                        winbar = 100,
+                    }
                 },
                 sections = {
-                    lualine_c = { {
-                        "diagnostic-message",
-                        icons = {
-                            error = "",
-                            warn = "",
-                            info = "",
-                            hint = "",
-                        },
-                        -- Replace '\n' by the separator
-                        line_separator = ". ",
-                        -- Only show the first line of diagnostic message
-                        first_line_only = false,
-                    } },
-                }
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { { 'filename', path = 1 },
+                        {
+                            "diagnostic-message",
+                            icons = {
+                                error = "",
+                                warn = "",
+                                info = "",
+                                hint = "",
+                            },
+                            -- Replace '\n' by the separator
+                            line_separator = ". ",
+                            -- Only show the first line of diagnostic message
+                            first_line_only = false,
+                        } },
+                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' }
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'location' },
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {},
             })
         end
     },
